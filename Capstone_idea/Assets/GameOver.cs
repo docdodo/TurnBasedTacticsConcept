496efc1bbebe2f251bdd07e8c2feb79d9830f27e
Robert <robert.blank.wardo@gmail.com>
2018-11-15 15:29:56 -0500

    Turns working, no combat
    adding combat later

---
+++ Capstone_idea/Assets/GameOver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+// whenever a player dies they come here and are allowed to restart or quit the game.
+public class GameOver : MonoBehaviour
+{
+    public void StruggleOn()
+        {
+        SceneManager.LoadScene(0);
+        }
+
+    public void Surrender()
+    {
+        Application.Quit();
+    }
+}
---
+++ Capstone_idea/Assets/Resources/PlayerMove.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// based on tactic move. Uses the mouse and raycasts as input to tell the character where to go.
+public class PlayerMove : TacticsMove
+{
+    GameObject Enemy;
+   
+    // Use this for initialization
+    void Start ()
+    {
+        Init();
+      
+        maxhp = 100;
+        hp = maxhp;
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.DrawRay(transform.position, transform.forward);
+        if (hp <=0)
+        {
+            SceneManager.LoadScene(1);
+
+        }
+        if(!turn)
+        {
+            myhitbox.SetActive(false);
+            return;
+        }
+
+        if (!moving)
+        {
+            FindSelectableTiles();
+            CheckMouse();
+        }
+        else
+        {
+            Move();
+            myhitbox.SetActive(true);
+
+        }
+
+
+	}
+    void CheckMouse()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "tile")
+                {
+                    tile t = hit.collider.GetComponent<tile>();
+                    if (t.selectable)
+                    {
+                        MovetoTile(t);
+                        
+                    }
+                }
+                 
+            }
+        }
+    }
+    
+
+    
+
+}
